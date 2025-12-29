a02c493fda6d0119b47f8f90cf95fb7557dd02d4
ErVaaL <ErVaLofficial@protonmail.com>
2025-12-29 20:44:25 +0100

    Setup webpack federation and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 
-const RemoteQueryApp = React.lazy(() => import('remoteQuery/App'));
-const RemoteReportApp = React.lazy(() => import('remoteReport/App'));
+const RemoteQueryApp = React.lazy(() => import("remoteQuery/App"));
+const RemoteReportApp = React.lazy(() => import("remoteReport/App"));
 
 export default function App() {
   return (
     <div style={{ padding: 16 }}>
-      <h1>Host</h1>
-
+      <h1 className="flex grow text-center items-center justify-center">Host</h1>
       <Suspense fallback={<div>Loading Query…</div>}>
         <RemoteQueryApp />
       </Suspense>
 
-      <hr style={{ margin: '16px 0' }} />
+      <hr style={{ margin: "16px 0" }} />
 
       <Suspense fallback={<div>Loading Report…</div>}>
         <RemoteReportApp />
@@ -20,4 +19,3 @@ export default function App() {
     </div>
   );
 }
-
